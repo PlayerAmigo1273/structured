fccc632b6a882e35d87505641b90b438b1b5a178
PlayerAmigo1273 <pedrocanadavive2@gmail.com>
2025-10-05 22:10:31 +0100

    example

---
+++ hash_map/example/cmp.sh
@@ -0,0 +1,7 @@
+
+#!/bin/bash
+
+cd ..
+cd ..
+
+gcc -g src/structured.c hash_map/status.c hash_map/example/main.c -o hash_map/example/main.exe
--- hash_map/example/main.c
+++ hash_map/example/main.c
@@ -19,8 +19,29 @@ uint64_t fnv1a_hash(const char * str) {
 #include "../hash_map_src.c"
 
 int main(void) {
-	int * wild;
-	*wild = 2;
+	int error;
+
+	struct structured_ccharp_int_hash_map map;
+	structured_ccharp_int_hash_map_create(&map, 5, (struct structured_ccharp_int_hash_node [5]) {});
+
+	// Insert element with key "A" and value 21. If an element with the same key is found, do not overwrite, instead:
+	// return error STRUCTURED_ERROR_DUPLICATE. If the map is full, return error STRUCTURED_ERROR_FULL.
+	error = structured_ccharp_int_hash_map_ins(&map, "A", 21, false);
+	if (error) {
+		fprintf(stderr, "Error inserting element: %s\n", structured_strerror(error));
+		return -1;
+	}
+
+	// Print nodes formatted
+	structured_ccharp_int_hash_map_printf(&map, "%s", "%i");
+
+	// Delete element with key "A". If the element is not found (has already been deleted or never existed),
+	// return error STRUCTURED_ERROR_NOT_FOUND.
+	error = structured_ccharp_int_hash_map_del(&map, "A");
+	if (error) {
+		fprintf(stderr, "Error deleting element: %s\n", structured_strerror(error));
+		return -1;
+	}
 
 	return 0;
 }
